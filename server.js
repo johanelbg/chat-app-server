ee87ff9beae0315ffed6ce1f8894e03817443119
johanelbg <johan.elberg38@gmail.com>
2019-05-03 14:31:27 +0300

    tent ENV

--- server.js
+++ server.js
@@ -3,11 +3,15 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const db = require("./config/db");
 const app = express();
+const dotenv = require("dotenv");
 
 const room = require("./app/routes/room");
 const chat = require("./app/routes/chat");
 
 const port = 8000;
+
+dotenv.config();
+console.log(`ENV ${process.env.NODE_ENV}`); // 8626
 
 app.use(
   bodyParser.urlencoded({
