a5a2c87655a4e18a801aa4d1f1f8548d1c5fd604
johanelbg <johan.elberg38@gmail.com>
2019-05-03 19:56:58 +0300

    wip

--- server.js
+++ server.js
@@ -3,17 +3,11 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const db = require("./config/db");
 const app = express();
-const dotenv = require("dotenv");
-const http = require("http");
-const url = require("url");
 
 const room = require("./app/routes/room");
 const chat = require("./app/routes/chat");
 
-const port = 8000;
-
-dotenv.config();
-console.log(`ENV ${process.env.NODE_ENV}`); // 8626
+const port = process.env.PORT || 8000;
 
 app.use(
   bodyParser.urlencoded({
@@ -28,8 +22,8 @@ app.use("/chat", chat);
 mongoose
   .connect(db.url, { useNewUrlParser: true })
   .then(() =>
-    app.listen(process.env.PORT, () => {
-      console.log("We are live on " + process.env.PORT);
+    app.listen(port, () => {
+      console.log("We are live on " + port);
     })
   )
   .catch(err => console.log(`connection failed: ${err}`));
