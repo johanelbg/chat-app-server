4ed58306502bdaab306f232ebfd056137bf6b97f
johanelbg <johan.elberg38@gmail.com>
2019-05-18 23:46:02 +0200

    added socket io support

--- app/routes/chat.js
+++ app/routes/chat.js
@@ -1,6 +1,23 @@
 const express = require("express");
 const router = express.Router();
 const Chat = require("../models/Chat.js");
+const server = require("http").Server(express);
+const io = require("socket.io")(server);
+
+const port = process.env.PORT || 4000;
+
+server.listen(port);
+
+io.on("connection", function(socket) {
+  console.log("User connected");
+  socket.on("disconnect", function() {
+    console.log("User disconnected");
+  });
+  socket.on("save-message", function(data) {
+    console.log(data);
+    io.emit("new-message", { message: data });
+  });
+});
 
 router.get("/", function(req, res, next) {
   Chat.find((err, products) => {
